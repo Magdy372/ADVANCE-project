92acc95e7f949c4a7f16a75d38319f54dd02baad
zahran4 <mahmoudhousam584@gmail.com>
2024-03-17 19:16:09 +0200

    redirect

--- adv/src/main/java/com/adv/adv/controller/UserController.java
+++ adv/src/main/java/com/adv/adv/controller/UserController.java
@@ -103,7 +103,7 @@ public ModelAndView login(@ModelAttribute("user") User loginUser, Model model, H
             // Save user ID in the session
             session.setAttribute("id", user.getId());
             if(user.getUserType()==User.UserType.ADMIN){
-                mav.setView(new RedirectView("/adminPage/addAdmin"));
+                mav.setView(new RedirectView("/admin/dashboard"));
             } else {
                 mav.setView(new RedirectView("/"));
             }
