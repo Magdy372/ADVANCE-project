9cf58e83346c55214ef6f7cb996e2a146970d377
zahran4 <mahmoudhousam584@gmail.com>
2024-03-20 15:41:18 +0200

    validate add category

--- adv/src/main/java/com/adv/adv/controller/CaregoryController.java
+++ adv/src/main/java/com/adv/adv/controller/CaregoryController.java
@@ -3,7 +3,9 @@ package com.adv.adv.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import com.adv.adv.model.Category;
 import com.adv.adv.repository.CategoryRepository;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categories")
@@ -37,12 +40,26 @@ public RedirectView deleteCtaegory(@PathVariable("Id") int Id) {
     this.categoryrepository.deleteById(Id);
     return new RedirectView("/categories");
 }
+
 @PostMapping("/create")
-public RedirectView addCategory(@RequestParam("name") String name) {
+public ModelAndView addCategory(@Valid @ModelAttribute("category") Category categor, BindingResult result,
+                                @RequestParam("name") String name) {
+    ModelAndView modelAndView = new ModelAndView();
+    
+    if (result.hasErrors()) {
+        modelAndView.setViewName("CategoriesList.html");
+        modelAndView.addObject("categories", categoryrepository.findAll());
+        modelAndView.addObject("errors", result.getAllErrors());
+        return modelAndView;
+    }
+
     Category category = new Category();
     category.setName(name);
     this.categoryrepository.save(category);
-    return new RedirectView("/categories");
+    
+    modelAndView.setViewName("redirect:/categories"); // Redirect to the categories page
+    return modelAndView;
 }
+
 
 }
--- adv/src/main/java/com/adv/adv/model/Category.java
+++ adv/src/main/java/com/adv/adv/model/Category.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+    @NotBlank(message = "Name cannot be null or empty")
+      @Pattern(regexp = "^(?=.*[a-zA-Z])[a-zA-Z\\s]+$", message = "Name cannot contain special characters and should have at least one alphabet")
     public String name;
     public Category() {
     }
