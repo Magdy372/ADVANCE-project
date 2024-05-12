cf067e093e204e78811b5ce0dd031160ed6d3cf7
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2024-05-13 01:54:45 +0300

    LIVE CHATTING

---
+++ adv/src/main/java/com/adv/adv/config/webSocketEventListener.java
@@ -0,0 +1,34 @@
+package com.adv.adv.config;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.adv.adv.controller.ChatMessage;
+import com.adv.adv.controller.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class webSocketEventListener {
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/chat", chatMessage);
+        }
+    }
+}
---
+++ adv/src/main/java/com/adv/adv/controller/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.adv.adv.controller;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+}
