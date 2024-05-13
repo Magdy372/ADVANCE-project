e2897f2da1687d4952d54a23545b13f7309ec932
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2024-05-13 18:06:34 +0300

    cart

---
+++ adv/src/main/java/com/adv/adv/model/Cart.java
@@ -0,0 +1,99 @@
+package com.adv.adv.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double sub_total;
+    private int quantity;
+    private double totalPrice; // Add this field
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @Transient
+    private double productPrice; // Transient field to hold the price of the product
+
+    public Cart() {
+    }
+
+    public Cart(Long id,double productPrice, double sub_total, int quantity, Product product, User user) {
+        this.id = id;
+        this.sub_total = sub_total;
+        this.quantity = quantity;
+        this.product = product;
+        this.user = user;
+        this.productPrice = product.getPrice(); // Set the product price during Cart creation
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public double getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public double getSub_total() {
+        return this.sub_total;
+    }
+
+    public void setSub_total(double sub_total) {
+        this.sub_total = sub_total;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+     public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+
+}
