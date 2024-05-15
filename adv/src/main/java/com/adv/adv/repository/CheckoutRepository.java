230056e4feb266f203ea6a0e039a172c8367f0a1
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2024-05-15 13:02:27 +0300

    database

---
+++ adv/src/main/java/com/adv/adv/repository/CheckoutRepository.java
@@ -0,0 +1,7 @@
+package com.adv.adv.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.adv.adv.model.Checkout;
+
+public interface CheckoutRepository extends JpaRepository<Checkout,Long>{
+
+}
